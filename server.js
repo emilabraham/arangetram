91ca88aa83855921d235a0e9ccfc238cd57a60c6
Emil Abraham <emilabraham1414@gmail.com>
2016-07-19 16:22:12 -0400

    Add about and contact page
    Add the basic framework for an about page and contact page. Also changed the CSS
    of the nav bar so that the text scaled better with smaller screens.

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
 var express = require('express')
   , logger = require('morgan')
   , app = express()
-  , template = require('pug').compileFile(__dirname + '/source/templates/homepage.pug')
+  , homepage = require('pug').compileFile(__dirname + '/source/templates/homepage.pug')
+  , contact = require('pug').compileFile(__dirname + '/source/templates/contact.pug')
+  , about = require('pug').compileFile(__dirname + '/source/templates/about.pug')
 
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
 
 app.get('/', function (req, res, next) {
   try {
-    var html = template({ title: 'Home' })
+    var html = homepage()
+    res.send(html)
+  } catch (e) {
+    next(e)
+  }
+})
+
+app.get('/contact', function (req, res, next) {
+  try {
+    var html = contact()
+    res.send(html)
+  } catch (e) {
+    next(e)
+  }
+})
+
+app.get('/about', function (req, res, next) {
+  try {
+    var html = about()
     res.send(html)
   } catch (e) {
     next(e)
